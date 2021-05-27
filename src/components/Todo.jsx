f07d46822f15925e6dec1c20993ada1e8b6b8c85
Maulik Shah <maulik.shah715@gmail.com>
2021-05-27 12:41:41 +0530

    🥼 Added the edit option, so now we can edit the text of the items in a Modal

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,54 @@
 import {
+  Button,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
+  makeStyles,
   Modal,
 } from "@material-ui/core";
 import React, { useState } from "react";
 import db from "../firebase";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const Todo = (props) => {
   const [open, setOpen] = useState(false);
+  const [input, setInput] = useState("");
+  const classes = useStyles();
+
+  const updateItem = (e) => {
+    db.collection("todos").doc(props.text.id).set(
+      {
+        todo: input,
+      },
+      { merge: true }
+    );
+
+    setOpen(false);
+  };
 
   return (
     <>
       <Modal open={open} onClose={(e) => setOpen(false)}>
-        <div>
+        <div className={classes.paper}>
           <h1>I am a modal</h1>
-          <button onClick={(e) => setOpen(false)}>Save</button>
+          <input
+            placeholder={props.text.todo}
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button onClick={updateItem}>Update</Button>
         </div>
       </Modal>
       <List>
